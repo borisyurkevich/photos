14134bec45a48a5d58fa104d254ff6a2e486a455
Boris Yurkevich <boris.yurkevich@gmail.com>
2017-08-28 23:12:37 +0100

    Setup UI for collection view.

---
+++ SOLID Photo Gallery/SOLID Photo Gallery/Photo Collection/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  SOLID Photo Gallery
+//
+//  Created by Boris Yurkevich on 28/08/2017.
+//  Copyright © 2017 Boris Yurkevich. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
