2adcbd04ec221810b51edbade6ee050dc37b4ec7
Boris Yurkevich <boris.yurkevich@gmail.com>
2017-08-28 22:47:48 +0100

    Organised.

---
+++ SOLID Photo Gallery/SOLID Photo Gallery/Support Files/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  SOLID Photo Gallery
+//
+//  Created by Boris Yurkevich on 28/08/2017.
+//  Copyright © 2017 Boris Yurkevich. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+}
+
